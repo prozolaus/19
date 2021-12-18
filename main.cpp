e1b121918077d7cc9144212181e5936eb68ace3f
Nikolai Prozorov <prozorovmykola@gmail.com>
2021-12-19 00:21:29 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include "std_lib_facilities.h"
+
+int main()
+try
+{
+}
+catch (const std::exception &e)
+{
+    std::cerr << "Exception: " << e.what() << '\n';
+    return 1;
+}
+catch (...)
+{
+    cerr << "Some kind of exception\n";
+    return 2;
+}
